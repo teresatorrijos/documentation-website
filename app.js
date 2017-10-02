8fd926784063899d95c89863f19729f31a910cd8
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-10-02 15:53:47 +0200

    adding icons

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express      = require('express');
 const path         = require('path');
-const favicon      = require('serve-favicon');
 const layouts      = require('express-ejs-layouts');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// default value for title local
 app.locals.title = 'Documentation Website';
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
@@ -21,20 +15,16 @@ app.use(layouts);
 const index = require('./routes/index');
 app.use('/', index);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
